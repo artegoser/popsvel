40187817eb86ecfe132ed5420c87f3ddffcebd54
Artemy Egorov <artegoser@gmail.com>
2025-06-11 15:55:56 +0300

    remove unused packages

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,8 @@
-import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-	preprocess: [vitePreprocess(), mdsvex()],
+	preprocess: [vitePreprocess()],
 	kit: { adapter: adapter() },
 	extensions: ['.svelte', '.svx']
 };
